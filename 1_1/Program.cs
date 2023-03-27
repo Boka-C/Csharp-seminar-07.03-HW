860fc436b965436d51f792c72304a2c228d10a82
Boka <boris.enuqashvili@gmail.com>
2023-03-27 20:54:54 +0400

    XZ

---
+++ 1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    является ли первое число квадратом второго.
+
+int num1 = 0;
+int num2 = 0;
+
+Console.Write("Please enter the number 1:");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter the second number");
+num2 = int.Parse(Console.ReadLine);
